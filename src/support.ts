87f873f9f033ae73cddb5771820a335bcee6e56b
Denys Dovhan <denysdovhan@gmail.com>
2020-12-19 15:12:14 +0200

    Add realTouch command

--- src/support.ts
+++ src/support.ts
@@ -2,8 +2,10 @@ import { realClick } from "./commands/realClick";
 import { realHover } from "./commands/realHover";
 import { realPress } from "./commands/realPress";
 import { realType } from "./commands/realType";
+import { realTouch } from './commands/realTouch';
 
 Cypress.Commands.add("realClick", { prevSubject: true }, realClick);
 Cypress.Commands.add("realHover", { prevSubject: true }, realHover);
+Cypress.Commands.add("realTouch", { prevSubject: true }, realTouch);
 Cypress.Commands.add("realPress", realPress);
 Cypress.Commands.add("realType", realType);
