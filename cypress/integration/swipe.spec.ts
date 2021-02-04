457f6d1dbefe19611e8bd7a021b1b0e341aa9070
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-02-04 16:17:04 +0200

    feat: add more options and more steps for realSwipe (#21)

--- cypress/integration/swipe.spec.ts
+++ cypress/integration/swipe.spec.ts
@@ -1,35 +1,41 @@
 describe("cy.realSwipe", () => {
   beforeEach(() => {
+    cy.viewport("iphone-x")
     cy.visit("https://csb-dhe0i-qj8xxmx8y.vercel.app/");
   });
 
   ([
     {
       button: "left",
-      swipe: "toRight",
+      swipe: "toLeft",
       touchPosition: "right",
     },
     {
       button: "right",
-      swipe: "toLeft",
+      swipe: "toRight",
       touchPosition: "left",
     },
     {
       button: "top",
-      swipe: "toBottom",
+      swipe: "toTop",
       touchPosition: "center",
     },
     {
       button: "bottom",
-      swipe: "toTop",
+      swipe: "toBottom",
       touchPosition: "top",
     },
   ] as const).forEach(({ button, swipe, touchPosition }) => {
     it(`swipes ${button} drawer ${swipe}`, () => {
       cy.contains("button", button).click();
-      cy.get(".MuiDrawer-paper").realSwipe(swipe, { touchPosition });
+      cy.get(".MuiDrawer-paper").realSwipe(swipe, { length: 150, step: 10, touchPosition });
 
       cy.get(".MuiDrawer-paper").should("not.be.visible");
     });
+  });
+
+  it("opens drawer with swipe", () => {
+    cy.get('.jss3.jss4').realSwipe("toRight", { length: 150, step: 10, touchPosition: "center" });
+    cy.get('.MuiDrawer-paper').realSwipe("toLeft", { length: 150, step: 10, touchPosition: "center" });
   });
 });
