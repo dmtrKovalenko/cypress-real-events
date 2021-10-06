b3436f157979a264bdb95a804b503cadddd14909
Fernando Barajas <fernyb@fernyb.net>
2021-10-05 21:22:53 -0700

    more updates

--- cypress/integration/wheel.spec.ts
+++ cypress/integration/wheel.spec.ts
@@ -1,12 +1,12 @@
 describe("cy.realMouseWheel", () => {
   function assertWheel(deltax: string, deltay:string, trusted: string, className: string) {
-    cy.get(".deltax").should("have.text", deltax);
-    cy.get(".deltay").should("have.text", deltay);
+    cy.get(".deltax").should("have.text", deltax, "DeltaX");
+    cy.get(".deltay").should("have.text", deltay, "DeltaY");
     cy.get(".isTrusted").should("have.text", trusted);
     cy.get(".target").should("have.text", className);
   }
 
-  it("scroll box", () => {
+  it("scroll over main", () => {
     cy.visit("./cypress/fixtures/scroll-test.html");
     cy.get(".main").realMouseWheel({ position: "center", deltaY: 500 });
     assertWheel("0", "500", "true", "main");
@@ -26,14 +26,43 @@ describe("cy.realMouseWheel", () => {
     cy.get(".main").realMouseWheel({ deltaX: 5, deltaY: 5 });
     assertWheel("5", "5", "true", "main");
 
-    cy.get(".main").realMouseWheel({ position: { x: 2, y: 2 }, deltaX: 11, deltaY: 12 });
-    assertWheel("11", "12", "true", "main");
+    cy.get(".main").realMouseWheel({ position: { x: 10, y: 10 }, deltaX: 111, deltaY: 12 });
+    assertWheel("111", "12", "true", "main");
+
+    cy.get(".main").realMouseWheel({ position: { x: 13, y: 13 }, deltaX: 22, deltaY: 33 });
+    assertWheel("22", "33", "true", "main");
+  });
+
+  it("scroll over smallbox", () => {
+    cy.get(".smallbox").realMouseWheel({ deltaX: 777, deltaY: 888 });
+    assertWheel("777", "888", "true", "smallbox");
 
     cy.get(".main").then(($main) => {
-      const x = $main.width() - 10;
-      const y = 10;
-      cy.get(".main").realMouseWheel({ position: { x, y }, deltaX: 8, deltaY: 9 });
-      assertWheel("8", "9", "true", "smallbox");
+      const y = 20;
+
+      cy.get(".main").realMouseWheel({ position: { x: $main.width() - 70, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "smallbox");
+
+      cy.get(".main").realMouseWheel({ position: { x: 35, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "main");
+
+      cy.get(".main").realMouseWheel({ position: { x: $main.width() - 51, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "smallbox");
+
+      cy.get(".main").realMouseWheel({ position: { x: 35, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "main");
+
+      // this one is not valid but pases in headless electron.
+      cy.get(".main").realMouseWheel({ position: { x: $main.width() - 110, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "smallbox");
+
+      cy.get(".main").realMouseWheel({ position: { x: 35, y }, deltaX: 10, deltaY: 10 });
+      assertWheel("10", "10", "true", "main");
+
+      // this is valid but fails in headless electron.
+      // cy.get(".main").realMouseWheel({ scrollBehavior: false, position: { x: $main.width() - 30, y }, deltaX: 10, deltaY: 10 });
+      // assertWheel("10", "10", "true", "smallbox");
+      // stange behavior when its in headless
     });
   });
 });
