769273df38da5c527b5b973cac32b81dd1d68742
Ilfat Mindubaev <yagarantuz@gmail.com>
2023-07-17 20:40:36 +0300

    feat: Add shiftKey modifier to keyboard events  (#514)
    * add shiftKey option to some events
    
    * add test cases
    
    * revert unrelated formatting changes
    
    * add shiftKey option for mouseUp event
    
    * one more formatting revert
    
    ---------
    
    Co-authored-by: Dmitriy <dmtr.kovalenko@outlook.com>

--- src/commands/mouseDown.ts
+++ src/commands/mouseDown.ts
@@ -5,6 +5,7 @@ import {
   Position,
 } from "../getCypressElementCoordinates";
 import { mouseButtonNumbers } from "../mouseButtonNumbers";
+import { keyToModifierBitMap } from "../keyToModifierBitMap";
 
 export interface realMouseDownOptions {
   /** Pointer type for realMouseDown, if "pen" touch simulated */
@@ -24,6 +25,11 @@ export interface realMouseDownOptions {
    * @default "left"
    */
   button?: keyof typeof mouseButtonNumbers;
+  /**
+   * Indicates whether the shift key was pressed or not when an event occurred
+   * @example cy.realMouseDown({ shiftKey: true });
+   */
+  shiftKey?: boolean;
 }
 
 /** @ignore this, update documentation for this function at index.d.ts */
@@ -51,6 +57,7 @@ export async function realMouseDown(
     buttons: mouseButtonNumbers[options.button ?? "left"],
     pointerType: options.pointer ?? "mouse",
     button: options.button ?? "left",
+    modifiers: options.shiftKey ? keyToModifierBitMap.Shift : 0,
   });
 
   log.snapshot("after").end();
