b9b4736912d20fd3b4b80f4fa91260e779312410
Dmitriy <dmtr.kovalenko@outlook.com>
2023-07-17 18:16:35 +0200

    Make cypress config cjs

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'cypress'
+const defineConfig = require("cypress").defineConfig;
 
-export default defineConfig({
+module.exports = defineConfig({
   video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
-})
+});
