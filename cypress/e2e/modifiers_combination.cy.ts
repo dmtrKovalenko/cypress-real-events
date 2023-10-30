da1d7b03bbc22da410ee60f7430dfc424b13ea6a
Svetlana Napris <svetlana.napris@elucid.com>
2023-10-30 13:29:11 -0400

    address the ESLint errors

--- cypress/e2e/modifiers_combination.cy.ts
+++ cypress/e2e/modifiers_combination.cy.ts
@@ -5,9 +5,9 @@ describe("Events behavior with modifiers combination applied", () => {
 
   const modifierKeys = ["shiftKey", "altKey", "ctrlKey"];
 
-  function createModifierObject(modifierKeys: any) {
+  function createModifierObject(modifierKeys: string[]): Record<string, boolean> {
     const options = {}; 
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       options[key] = true; 
     }   
     return options;
@@ -19,35 +19,35 @@ describe("Events behavior with modifiers combination applied", () => {
   // @see https://app.circleci.com/pipelines/github/dmtrKovalenko/cypress-real-events/842/workflows/1f98e690-ec69-4dcf-9e0e-3f8eb67dc709/jobs/2332/parallel-runs/0?filterBy=ALL
   it.skip(`detects ${modifierKeys} modifiers on click`, () => {
     cy.get("#action-button").realClick(createModifierObject(modifierKeys));
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       cy.contains(`${key} was pressed`); 
     }
   });
 
   it(`detects ${modifierKeys} modifiers on hover`, () => {
     cy.get("#mouse-move-div").realHover(createModifierObject(modifierKeys));
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       cy.contains(`${key} was pressed`); 
     }
   });
 
   it(`detects ${modifierKeys} modifiers on mousedown`, () => {
     cy.get("#mouse-down-div").realMouseDown(createModifierObject(modifierKeys));
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       cy.contains(`${key} was pressed`); 
     }
   });
 
   it(`detects ${modifierKeys} modifiers on mpuseup`, () => {
     cy.get("#mouse-up-div").realMouseUp(createModifierObject(modifierKeys));
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       cy.contains(`${key} was pressed`); 
     }
   });
 
   it(`detects ${modifierKeys} modifiers on mousemove`, () => {
     cy.get("#mouse-move-div").realMouseMove(100, 50, createModifierObject(modifierKeys));
-    for (let key of modifierKeys) {
+    for (const key of modifierKeys) {
       cy.contains(`${key} was pressed`); 
     }
   });
